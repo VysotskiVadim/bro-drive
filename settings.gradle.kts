872e1900a71ab003528c7a5aa264e90cd5d5e43b
Vadzim Vysotski <vadzim.vysotski@mapbox.com>
2024-03-21 11:32:12 +0100

    setup repo and projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,13 +6,26 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = System.getenv("MAPBOX_DOWNLOADS_TOKEN")
+            }
+        }
     }
 }
 
 rootProject.name = "Bro drive"
-include(":app")
 include(":bro-drive-app")
